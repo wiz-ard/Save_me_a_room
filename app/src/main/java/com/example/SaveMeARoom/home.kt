6a3aee68b087a21ec41791c382c0d5c22702e295
Zachary Surla <zmsurla00@gmail.com>
2023-02-01 15:23:40 -0600

    recycle view work for reservations almost done

--- app/src/main/java/com/example/SaveMeARoom/home.kt
+++ app/src/main/java/com/example/SaveMeARoom/home.kt
@@ -3,20 +3,25 @@ package com.example.SaveMeARoom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.homefragment.*
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // setting variables to each of the fragment layout views
         val homeFragment = Homefragment()
         val mapFragment = Mapfragment()
         val reservationFragment = Reservationfragment()
         val profileFragment = Profilefragment()
 
+        //sets the initial fragment for when first launched
         setCurrentFragment(homeFragment)
 
+        // bottom navigation bar click listener
         bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.Home -> setCurrentFragment(homeFragment)
@@ -27,8 +32,11 @@ class home : AppCompatActivity() {
             true
         }
 
+
+
     }
 
+    //function for navigation bar to change fragments when clicked
     private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply{
             replace(R.id.flFragment, fragment)
--- app/src/main/java/com/example/SaveMeARoom/homefragment.kt
+++ app/src/main/java/com/example/SaveMeARoom/homefragment.kt
@@ -4,4 +4,17 @@ import androidx.fragment.app.Fragment
 
 class Homefragment: Fragment(R.layout.homefragment) {
 
+    // building list to be replaced with api eventually
+    var buildingList = mutableListOf(
+        buildingname("IESB"),
+        buildingname("COBB"),
+        buildingname("GTM"),
+        buildingname("Neth"),
+        buildingname("BOUG")
+    )
+    //recycle view adaptor settings for filling the view
+    //val adapter = homeRecycleAdaptor(buildingList)
+    //rvReservation.adapter = adapter
+    //rvReservation.layoutManager = LinearLayoutManager(this)
+
 }
