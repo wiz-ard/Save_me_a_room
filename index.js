0cc6992acde1ff9990acd139907ae45c2cb59f17
Nimbus414 <71732514+Nimbus414@users.noreply.github.com>
2023-02-07 18:41:01 -0600

    Update index.js
    Got it working in the browser instead of from the command line

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const process = require('process');
 
 // inputs are wrapped in double quotes and string values are wrapped in single quotes
 app.get("/", function(req, res) {
-    let sql = process.argv[2]
+    res.send("Hello!")
+});
+
+app.get("/search", function(req, res) {
+    // req.params.query has %20 instead of spaces
+    // need to find all instances of %20 and replace them with spaces
+    // then I can set the new req to sql and it should fire
+    let sql = req.query.query;
+
     connection.query(sql, "admin", function(err, results) {
         if (err) throw err;
         res.send(results);
