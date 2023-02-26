c5109b95dd99831980c7b3953a81db56c030e7d5
Terry T <62068654+teter2000@users.noreply.github.com>
2023-02-26 15:33:07 -0600

    Got inserting and filtering to work

--- app/src/main/java/com/example/SaveMeARoom/RoomSelection.kt
+++ app/src/main/java/com/example/SaveMeARoom/RoomSelection.kt
@@ -3,18 +3,19 @@ package com.example.SaveMeARoom
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.StrictMode
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.net.URL
 
 class RoomSelection : AppCompatActivity() {
 
     private lateinit var adaptor : roomRecycleAdaptor
     private lateinit var roomRecycleView : RecyclerView
     private lateinit var roomList : ArrayList<roomData>
-    private lateinit var roomText : Array<String>
     private lateinit var room : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +25,20 @@ class RoomSelection : AppCompatActivity() {
         val buildingName = intent.getStringExtra("building name")
         val date = intent.getStringExtra("date")
         val time = intent.getStringExtra("time")
+        var start = ""
+        if(time.equals("5:00pm - 7:00pm")){
+            start = "17:00:00"
+        }else if(time.equals("7:00pm - 9:00pm")){
+            start = "19:00:00"
+        }else{
+            start = "21:00:00"
+        }
         val occupancy = intent.getStringExtra("occupancy")
 
         val topBuildingTitle : TextView = findViewById(R.id.tvRoomTitle)
         topBuildingTitle.text = buildingName+" reservation"
 
-        getRooms()
+        getRooms(buildingName.toString(),date.toString(),time.toString(),occupancy.toString(),start)
 
         roomRecycleView = findViewById(R.id.rvRoom)
         roomRecycleView.layoutManager = LinearLayoutManager(this)
@@ -54,17 +63,25 @@ class RoomSelection : AppCompatActivity() {
     }
 
 
-    private fun getRooms(){
-        roomText = arrayOf(
-            "iesb 1",
-            "iesb 2",
-            "iesb 3"
-        )
+    private fun getRooms(building:String,date:String,time:String,occupancy:String, start:String){
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+
+        val ip = "http://3.132.20.107:3000"
+
+        val query = "/search?query=SELECT%20DISTINCT%20locations.Room_Number%20FROM%20Capstone.locations,%20Capstone.reservations%20WHERE%20locations.Occupancy_Range%20=%20" + occupancy +"%20AND%20locations.Building_Name%20=%20%27" + building +"%27%20AND%20(locations.Room_Number%20Not%20IN%20(SELECT%20Room_Number%20FROM%20Capstone.reservations%20WHERE%20Start_Date_Time%20=%20%27" + date + "%20" + start + "%27%20OR%20End_Date_time%20LIKE%20%27" + date + "%2011:59:00%27))"
+
+        val url = URL(ip.plus(query))
+
+        val text = url.readText()
+
+        val rooms = text.split(",")
 
         roomList = arrayListOf()
 
-        for (i in roomText.indices){
-            val roomData = roomData(roomText[i])
+        for (i in rooms.indices){
+            val roomData = roomData(rooms[i].substringAfter(":").substringBefore('}'))
             roomList.add(roomData)
         }
 
