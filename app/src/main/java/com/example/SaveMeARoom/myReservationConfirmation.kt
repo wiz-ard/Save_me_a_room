9322de07b8fd9baccf222101032b991d2d83d59b
Terry T <62068654+teter2000@users.noreply.github.com>
2023-02-26 23:03:21 -0600

    messing w stuff

--- app/src/main/java/com/example/SaveMeARoom/myReservationConfirmation.kt
+++ app/src/main/java/com/example/SaveMeARoom/myReservationConfirmation.kt
@@ -2,9 +2,11 @@ package com.example.SaveMeARoom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.book_confirmation.*
+import java.net.URL
 
 class myReservationConfirmation : AppCompatActivity() {
 
@@ -13,6 +15,18 @@ class myReservationConfirmation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_reservation_confirmation)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+
+        val ip = "http://3.132.20.107:3000"
+
+        //val query = "/search?query=SELECT%20DISTINCT%20Building_Name%20FROM%20locations%20WHERE%20Associated_College=%27" + college + "%27%20OR%20Associated_College=%27General%27%20ORDER%20BY%20Building_Name"
+
+        //val url = URL(ip.plus(query))
+
+        //val text = url.readText()
 
         val buildingName = null
         val date = null
--- app/src/main/java/com/example/SaveMeARoom/reservationfragment.kt
+++ app/src/main/java/com/example/SaveMeARoom/reservationfragment.kt
@@ -2,19 +2,21 @@ package com.example.SaveMeARoom
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.create_account.*
+import java.net.URL
 
 class Reservationfragment: Fragment() {
 
     private lateinit var adaptor: myReservationRecycleAdaptor
     private lateinit var recycleView: RecyclerView
     private lateinit var myReservationList: ArrayList<myReservationData>
-    private lateinit var myReservationText: Array<String>
 
     // needed to display and fill in the data when its created
     override fun onCreateView(
@@ -27,8 +29,6 @@ class Reservationfragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
-
-        val bundle = arguments
 
         getReservations()
 
@@ -48,19 +48,29 @@ class Reservationfragment: Fragment() {
 
     private fun getReservations() {
 
-        myReservationText = arrayOf(
-            "Reservation 1",
-            "reservation 2",
-            "reservation 3"
-        )
+        val bundle = arguments
+
+        val email = bundle!!.getString("email")
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+
+        val ip = "http://3.132.20.107:3000"
+
+        val query = "/search?query=SELECT%20Building_Name,%20Room_Number,%20Start_Date_Time,%20End_Date_Time%20FROM%20Capstone.reservations%20WHERE%20Reserver_Email=%27" + email + "%27"
+
+        val url = URL(ip.plus(query))
+
+        val text = url.readText()
+
+        val reservations = text.split(",")
 
         myReservationList = arrayListOf()
 
-        for(i in myReservationText.indices){
-            myReservationList.add(myReservationData(myReservationText[i]))
+        for(i in reservations.indices){
+            val reservation = myReservationData(reservations[i].substringAfter(":").substringAfter('"').substringBefore('"'))
+            myReservationList.add(reservation)
         }
-
-
     }
-
 }
