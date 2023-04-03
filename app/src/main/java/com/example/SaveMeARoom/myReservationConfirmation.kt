0aad7fa8cb71a6c0003d93eff2918fa27569a24a
Nimbus414 <davidriser414@gmail.com>
2023-04-03 16:06:39 -0500

    Fixed the bug, club leader now works as intended

--- app/src/main/java/com/example/SaveMeARoom/myReservationConfirmation.kt
+++ app/src/main/java/com/example/SaveMeARoom/myReservationConfirmation.kt
@@ -102,7 +102,7 @@ class myReservationConfirmation : AppCompatActivity() {
 
             text = url.readText()
 
-            val pending = text.substringAfter(":").substringBefore("}")
+            val pending = text.substringAfter(":").substringAfter('"').substringBefore('"')
             //if pending and updating are both 0, then send to UpdateDateSelectionPage
             val intent = Intent(this, UpdateDateSelection::class.java)
             intent.putExtra("building name", buildingName)
