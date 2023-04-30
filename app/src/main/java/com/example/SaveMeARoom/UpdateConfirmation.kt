1e95608260a865cf9a46eeef345c3d10e88c61f8
Terry T <62068654+teter2000@users.noreply.github.com>
2023-04-30 17:59:27 -0500

    fixed updating reservation so you cant pick a time you already have one

--- app/src/main/java/com/example/SaveMeARoom/UpdateConfirmation.kt
+++ app/src/main/java/com/example/SaveMeARoom/UpdateConfirmation.kt
@@ -62,140 +62,147 @@ class UpdateConfirmation : AppCompatActivity() {
                 start = "21:00:00"
                 end = "23:00:00"
             }
+            val checkResQuery = "/search?query=SELECT%20*%20FROM%20reservations%20WHERE%20Reserver_Email=%27" + email + "%27%20AND%20Start_Date_Time=%27"  + date + "%20" + start + "%27"
+            val checkurl = URL(ip.plus(checkResQuery))
+            val checkText = checkurl.readText()
 
-            if(pending.equals("0") && updating.equals("0")){
-                //insert request to updates table
-                var query = "/search?query=INSERT%20INTO%20updates%20VALUES(" + resId + ",%27" + date + "%20" + start + "%27,%27" + date + "%20" + end + "%27)"
+            if(checkText.length > 2){
+                Toast.makeText(this, "You already have a reservation at this time.", Toast.LENGTH_SHORT).show()
+            }else{
+                if(pending.equals("0") && updating.equals("0")){
+                    //insert request to updates table
+                    var query = "/search?query=INSERT%20INTO%20updates%20VALUES(" + resId + ",%27" + date + "%20" + start + "%27,%27" + date + "%20" + end + "%27)"
 
-                var url = URL(ip.plus(query))
+                    var url = URL(ip.plus(query))
 
-                var text = url.readText()
+                    var text = url.readText()
 
-                //set updating to 1
-                query = "/search?query=UPDATE%20reservations%20SET%20Updating=1%20WHERE%20Reservation_Id=" + resId + ""
-                url = URL(ip.plus(query))
-                text = url.readText()
-                Toast.makeText(this, "Update request sent.", Toast.LENGTH_SHORT).show()
+                    //set updating to 1
+                    query = "/search?query=UPDATE%20reservations%20SET%20Updating=1%20WHERE%20Reservation_Id=" + resId + ""
+                    url = URL(ip.plus(query))
+                    text = url.readText()
+                    Toast.makeText(this, "Update request sent.", Toast.LENGTH_SHORT).show()
 
-                //log update
-                val curTime = LocalTime.now()
-                val curDate = LocalDate.now()
-                val logTime = curTime.toString() + " " + curDate.toString()
-                //getting initial reservation time
-                query =
-                    "/search?query=SELECT%20Start_Date_Time,End_Date_Time%20FROM%20reservations%20WHERE%20Reservation_Id=%27" + resId + "%27"
+                    //log update
+                    val curTime = LocalTime.now()
+                    val curDate = LocalDate.now()
+                    val logTime = curTime.toString() + " " + curDate.toString()
+                    //getting initial reservation time
+                    query =
+                        "/search?query=SELECT%20Start_Date_Time,End_Date_Time%20FROM%20reservations%20WHERE%20Reservation_Id=%27" + resId + "%27"
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                text = url.readText()
+                    text = url.readText()
 
-                val times = text.split(",")
+                    val times = text.split(",")
 
-                val initialDate = times[0].substringAfter(":").substringAfter("\"").substringBefore(" ")
+                    val initialDate = times[0].substringAfter(":").substringAfter("\"").substringBefore(" ")
 
-                val initialStart = times[0].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
+                    val initialStart = times[0].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
 
-                val initialEnd = times[1].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
+                    val initialEnd = times[1].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
 
-                val resTime = initialDate + " " + initialStart + " - " + initialDate + " " + initialEnd
+                    val resTime = initialDate + " " + initialStart + " - " + initialDate + " " + initialEnd
 
-                //inserting into logs
-                query =
-                    "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
+                    //inserting into logs
+                    query =
+                        "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                text = url.readText()
+                    text = url.readText()
 
-                // set viewing to false
-                query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
+                    // set viewing to false
+                    query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                url.readText()
+                    url.readText()
 
-                finish()
-            }else if(pending.equals("1") && updating.equals("0")){
-                //log update
-                val curTime = LocalTime.now()
-                val curDate = LocalDate.now()
-                val logTime = curTime.toString() + " " + curDate.toString()
+                    finish()
+                }else if(pending.equals("1") && updating.equals("0")){
+                    //log update
+                    val curTime = LocalTime.now()
+                    val curDate = LocalDate.now()
+                    val logTime = curTime.toString() + " " + curDate.toString()
 
-                val resTime = date + " " + start + " - " + date + " " + end
+                    val resTime = date + " " + start + " - " + date + " " + end
 
-                //inserting into logs
-                var query =
-                    "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
+                    //inserting into logs
+                    var query =
+                        "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
 
-                var url = URL(ip.plus(query))
+                    var url = URL(ip.plus(query))
 
-                var text = url.readText()
-                //modify time of pending request
-                query = "/search?query=UPDATE%20reservations%20SET%20Start_Date_Time=%27" + date + "%20" + start + "%27,End_Date_Time=%27" + date + "%20" + end + "%27%20WHERE%20Reservation_Id=" + resId + ""
+                    var text = url.readText()
+                    //modify time of pending request
+                    query = "/search?query=UPDATE%20reservations%20SET%20Start_Date_Time=%27" + date + "%20" + start + "%27,End_Date_Time=%27" + date + "%20" + end + "%27%20WHERE%20Reservation_Id=" + resId + ""
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                text = url.readText()
+                    text = url.readText()
 
-                Toast.makeText(this, "Pending request updated.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Pending request updated.", Toast.LENGTH_SHORT).show()
 
-                // set viewing to false
-                query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
+                    // set viewing to false
+                    query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                url.readText()
+                    url.readText()
 
-                finish()
-            }else if(pending.equals("0") && updating.equals("1")){
-                //modify update request
-                var query = "/search?query=UPDATE%20updates%20SET%20New_Start_Time=%27" + date + "%20" + start + "%27,New_End_Time=%27" + date + "%20" + end + "%27%20WHERE%20Reservation_Id=" + resId + ""
+                    finish()
+                }else if(pending.equals("0") && updating.equals("1")){
+                    //modify update request
+                    var query = "/search?query=UPDATE%20updates%20SET%20New_Start_Time=%27" + date + "%20" + start + "%27,New_End_Time=%27" + date + "%20" + end + "%27%20WHERE%20Reservation_Id=" + resId + ""
 
-                var url = URL(ip.plus(query))
+                    var url = URL(ip.plus(query))
 
-                var text = url.readText()
+                    var text = url.readText()
 
-                Toast.makeText(this, "Update request changed.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Update request changed.", Toast.LENGTH_SHORT).show()
 
-                //log update
-                val curTime = LocalTime.now()
-                val curDate = LocalDate.now()
-                val logTime = curTime.toString() + " " + curDate.toString()
-                //getting initial reservation time
-                query =
-                    "/search?query=SELECT%20Start_Date_Time,End_Date_Time%20FROM%20reservations%20WHERE%20Reservation_Id=%27" + resId + "%27"
+                    //log update
+                    val curTime = LocalTime.now()
+                    val curDate = LocalDate.now()
+                    val logTime = curTime.toString() + " " + curDate.toString()
+                    //getting initial reservation time
+                    query =
+                        "/search?query=SELECT%20Start_Date_Time,End_Date_Time%20FROM%20reservations%20WHERE%20Reservation_Id=%27" + resId + "%27"
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                text = url.readText()
+                    text = url.readText()
 
-                val times = text.split(",")
+                    val times = text.split(",")
 
-                val initialDate = times[0].substringAfter(":").substringAfter("\"").substringBefore(" ")
+                    val initialDate = times[0].substringAfter(":").substringAfter("\"").substringBefore(" ")
 
-                val initialStart = times[0].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
+                    val initialStart = times[0].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
 
-                val initialEnd = times[1].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
+                    val initialEnd = times[1].substringAfter(":").substringAfter("\"").substringBefore("\"").substringAfter(" ")
 
-                val resTime = initialDate + " " + initialStart + " - " + initialDate + " " + initialEnd
+                    val resTime = initialDate + " " + initialStart + " - " + initialDate + " " + initialEnd
 
-                //inserting into logs
-                query =
-                    "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
+                    //inserting into logs
+                    query =
+                        "/search?query=INSERT%20INTO%20reservationlogs%20VALUES(%27" + resId + "%27,%27" + buildingName + "%27,%27" + room + "%27,%27" + resTime + "%27,%27" + email + "%27,%27" + college + "%27,%27" + logTime + "%27,%27" + email + "%27,%27False%27,%27True%27,%27False%27,%27False%27,%27False%27,%27False%27,%27False%27)"
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                text = url.readText()
+                    text = url.readText()
 
 
-                // set viewing to false
-                query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
+                    // set viewing to false
+                    query = "/search?query=UPDATE%20reservations%20SET%20Viewing=0%20WHERE%20Reservation_Id=" + resId
 
-                url = URL(ip.plus(query))
+                    url = URL(ip.plus(query))
 
-                url.readText()
+                    url.readText()
 
-                finish()
+                    finish()
+                }
             }
         }
 
